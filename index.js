a8896cbc054cf9c7580854bb855cc055330a4e77
samundradahal <samundra1313@gmail.com>
2024-01-23 18:58:17 +0545

    Feburary 23 (Javascript Basics)

---
+++ index.js
@@ -0,0 +1,29 @@
+//Prints the message
+console.log("Hello Worlds");
+
+//Variables(var,let,const)
+//camelCase pattern for variable assignment
+
+var myName = "Samundra";
+myName = "Sagar"
+console.log(myName);
+
+let hello = "Ram";
+hello = "Shyam";
+console.log(hello);
+
+//Create 4 variables using const keyword for table , fan ,chair , pen
+
+const fanCompany = "Baltra";
+const tableType = "Dining Table";
+const chairType = "Armchair";
+const penName = "Cello pen";
+
+
+//Comments
+//Single line comment
+/* Multi
+Line 
+Comment */
+
+//use var and let keyword for four variables
